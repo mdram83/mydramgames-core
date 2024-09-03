1c42dff295c9451e99f49e0dd61f382806037652
mdram83 <michal.dramowicz@gmail.com>
2024-09-03 11:41:21 +0200

    PHPDocs update

--- src/GameOption/GameOptionType.php
+++ src/GameOption/GameOptionType.php
@@ -4,5 +4,9 @@ namespace MyDramGames\Core\GameOption;
 
 interface GameOptionType
 {
+    /**
+     * Provides value of specific type, e.g. 'checkbox' or 'radio'.
+     * @return int|string|null
+     */
     public function getValue(): int|string|null;
 }
--- src/GameOption/GameOptionValue.php
+++ src/GameOption/GameOptionValue.php
@@ -4,6 +4,15 @@ namespace MyDramGames\Core\GameOption;
 
 interface GameOptionValue
 {
+    /**
+     * Returns actual value
+     * @return int|string|null
+     */
     public function getValue(): int|string|null;
+
+    /**
+     * Returns label for user friendly messages (e.g. '1 Hour' instead of `3600`)
+     * @return string
+     */
     public function getLabel(): string;
 }
